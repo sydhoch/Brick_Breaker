a8b7002696d2489d94770bfb9617e0795f9fcbf8
Leah Schwartz <leah.schwartz@duke.edu>
2019-01-26 12:42:39 -0500

    basic game setup of scene and stage

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,66 @@
-public class Game {
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+
+
+
+
+public class Game extends Application{
+
+    public static final int SIZE = 400;
+    public static final Paint BACKGROUND = Color.CYAN;
+    public static final String TITLE = "Hello JavaFX";
+    public static final int FRAMES_PER_SECOND = 60;
+    public static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
+    public static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
+
+
+    private Scene myScene;
+    private Circle myCirc;
+
+    public void start (Stage stage) {
+        // attach scene to the stage and display it
+        myScene = setupGame(SIZE, SIZE, BACKGROUND);
+        stage.setScene(myScene);
+        stage.setTitle(TITLE);
+        stage.show();
+        // attach "game loop" to timeline to play it
+        var frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY), e -> step(SECOND_DELAY));
+        var animation = new Timeline();
+        animation.setCycleCount(Timeline.INDEFINITE);
+        animation.getKeyFrames().add(frame);
+        animation.play();
+    }
+
+    private Scene setupGame (int width, int height, Paint background) {
+        // create one top level collection to organize the things in the scene
+        var root = new Group();
+        // create a place to see the shapes
+        var scene = new Scene(root, width, height, background);
+        myCirc = new Circle(100);
+        myCirc.setFill(Color.DARKSALMON);
+        root.getChildren().add(myCirc);
+        return scene;
+    }
+
+    private void step (double elapsedTime) {
+        myCirc.setRotate(elapsedTime);
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
-public class Main {
-    public static void main(String[] args){
+import javafx.application.Application;
 
-    }
+import static javafx.application.Application.launch;
+
+public class Main {
 }
