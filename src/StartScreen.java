13a77cb471ca13df4a04bfe2ca163085423f8e82
SYDNEY HOCHBERG <srh57@duke.edu>
2019-01-31 12:40:52 -0500

    some work on setting up start screen

--- src/StartScreen.java
+++ src/StartScreen.java
@@ -3,33 +3,47 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 
 import java.util.ArrayList;
 
 public class StartScreen {
 
     private Scene myScene;
+    private int size;
 
     public StartScreen(){
-        myScene = setUpScreen();
-    }
-
-    /**
-     * Gets Scene created by GameSetUp
-     * @return game Scene
-     */
-    public Scene getScene(){
-        return myScene;
+        myScene = setUpScreen(size);
     }
 
 
-
-    private Scene setUpScreen(){
+    private Scene setUpScreen(int screenSize){
         Group root = new Group();
         var scene = new Scene(root, 100, 100, Color.BLUEVIOLET);
 
+        Text title = getTitle(screenSize);
+
+
+//        gameOverText.setText(message);
+//        gameOverText.setFont(new Font(20));
+//        gameOverText.setFill(color);
+//        gameOverText.setX(size / 2 - (gameOverText.getBoundsInLocal().getWidth() / 2));
+//        gameOverText.setY(size / 2);
 
         return scene;
+    }
+
+    private Text getTitle(int screenSize) {
+        Text title = new Text();
+        title.setFill(Color.BLACK);
+        title.setFont(new Font(screenSize / 25));
+        title.setFont(new Font(20));
+        title.setX(screenSize / 2);
+        title.setY(screenSize - title.getBoundsInLocal().getHeight());
+        title.setText("");
+        return title;
     }
 
 
