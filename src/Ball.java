e05ff2cf184304f3e981a258a3219e268334d949
SYDNEY HOCHBERG <srh57@duke.edu>
2019-02-11 17:43:04 -0500

    fixed merge conflicts NOW

--- src/Ball.java
+++ src/Ball.java
@@ -1,5 +1,4 @@
 import javafx.animation.Timeline;
-
 import java.util.Random;
 
 public class Ball extends Item {
@@ -12,96 +11,95 @@ public class Ball extends Item {
     private final String FAR_RIGHT = "far right";
     private final String MIDDLE_RIGHT = "middle right";
 
-    Ball(){
-    public Ball(){
-        setImage(BOUNCER_IMAGE);
-        setStartingVelocity();
-        setCanSee(true);
+    public Ball() {
+            setImage(BOUNCER_IMAGE);
+            setStartingVelocity();
+            setCanSee(true);
 
-    }
-
-    public static int getStartingXVelocity() {
-        return STARTING_X_VELOCITY;
-    }
-
-    public static int getStartingYVelocity() {
-        return STARTING_Y_VELOCITY;
-    }
-
-
-    /**
-     * makes the ball bounce off of the left right and top walls by switching the direction of X & Y velocities
-     * @param screenWidth width of screen
-     * @param screenHeight height of screen
-    */
-    public void bounce(double screenWidth, double screenHeight, Tests tester, Timeline animation){
-        System.out.println(getXVelocity());
-        System.out.println(getYVelocity() );
-        if(getXCoordinate() < 0 || getXCoordinate()>(screenWidth - getWidth())){
-            setXVelocity(getXVelocity() * -1);
-            if (tester!=null) {
-                tester.setFirstEvent("Corner Bounce Back");
-                animation.stop();
-                tester.callTest();
-            }
         }
-        if(getYCoordinate() < 0){
+
+        public static int getStartingXVelocity () {
+            return STARTING_X_VELOCITY;
+        }
+
+        public static int getStartingYVelocity () {
+            return STARTING_Y_VELOCITY;
+        }
+
+
+        /**
+         * makes the ball bounce off of the left right and top walls by switching the direction of X & Y velocities
+         * @param screenWidth width of screen
+         * @param screenHeight height of screen
+         */
+        public void bounce ( double screenWidth, double screenHeight, Tests tester, Timeline animation){
+            System.out.println(getXVelocity());
+            System.out.println(getYVelocity());
+            if (getXCoordinate() < 0 || getXCoordinate() > (screenWidth - getWidth())) {
+                setXVelocity(getXVelocity() * -1);
+                if (tester != null) {
+                    tester.setFirstEvent("Corner Bounce Back");
+                    animation.stop();
+                    tester.callTest();
+                }
+            }
+            if (getYCoordinate() < 0) {
+                setYVelocity(getYVelocity() * -1);
+                if (tester != null) {
+                    tester.setFirstEvent("Corner Bounce Back");
+                    animation.stop();
+                }
+            }
+
+        }
+
+        /**
+         * reverse direction of ball
+         */
+        public void bounceOffPad (String area){
+            if (getXVelocity() < 0) {
+                xDirectionChooser(FAR_RIGHT, MIDDLE_RIGHT, area);
+            }
+            if (getXVelocity() > 0) {
+                xDirectionChooser(FAR_LEFT, MIDDLE_LEFT, area);
+            }
             setYVelocity(getYVelocity() * -1);
-            if(tester!=null) {
-                tester.setFirstEvent("Corner Bounce Back");
-                animation.stop();
+        }
+
+        private void xDirectionChooser (String far, String middle, String area){
+            if (area.equals(far)) {
+                setXVelocity(getXVelocity() * -1);
+            }
+            if (area.equals(middle)) {
+                setXVelocity(getXVelocity() * -.75);//change later
             }
         }
 
-    }
 
-    /**
-     * reverse direction of ball
-     */
-    public void bounceOffPad(String area){
-        if (getXVelocity() < 0) {
-            xDirectionChooser(FAR_RIGHT,MIDDLE_RIGHT,area);
+        public void bounceOff () {
+            setYVelocity(getYVelocity() * -1);
+            setXVelocity(getXVelocity() * -1);
+
         }
-        if(getXVelocity()>0){
-            xDirectionChooser(FAR_LEFT,MIDDLE_LEFT,area);
+
+        /**
+         * Checks if ball is off of screen, indicating that it "fell"
+         * @param screenHeight height of scene
+         * @return if the ball is offscreen
+         */
+        public boolean ballFell ( double screenHeight){
+            return getYCoordinate() > (screenHeight) && canSee();
         }
-        setYVelocity(getYVelocity()*-1);
-    }
 
-    private void xDirectionChooser(String far, String middle, String area){
-        if(area.equals(far)){
-            setXVelocity(getXVelocity()*-1);
+        private void setStartingVelocity () {
+            setXVelocity(getStartingXVelocity());
+            setYVelocity(getStartingYVelocity());
         }
-        if(area.equals(middle)){
-            setXVelocity(getXVelocity()*-.75);//change later
+
+        public void teleport ( int screenSize){
+            Random rand = new Random();
+            placeItem(rand.nextInt(screenSize), rand.nextInt(screenSize));
         }
-    }
-
-
-    public void bounceOff(){
-        setYVelocity(getYVelocity() * -1);
-        setXVelocity(getXVelocity() * -1);
-
-    }
-
-    /**
-     * Checks if ball is off of screen, indicating that it "fell"
-     * @param screenHeight height of scene
-     * @return if the ball is offscreen
-     */
-    public boolean ballFell(double screenHeight){
-        return getYCoordinate() > (screenHeight) && canSee();
-    }
-
-    private void setStartingVelocity(){
-        setXVelocity(getStartingXVelocity());
-        setYVelocity(getStartingYVelocity());
-    }
-
-    public void teleport(int screenSize){
-        Random rand = new Random();
-        placeItem(rand.nextInt(screenSize), rand.nextInt(screenSize));
-    }
 
 
 }
